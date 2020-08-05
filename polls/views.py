27a0af1d17a54ea38d1d7e5608368e8a85d2a461
dangthihoa <dang.thi.hoa@sun-asterisk.com>
2020-08-05 14:41:41 +0700

    fix

--- polls/views.py
+++ polls/views.py
@@ -21,7 +21,7 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/details.html'
+    template_name = 'polls/detail.html'
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
